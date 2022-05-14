fa5cba0a5fc785effc9ef11891411c1b09af7497
ssti3r <ssti3r@REVISION-PC>
2022-05-14 06:56:09 +0300

    Добавьте файлы проекта.

---
+++ Bloknot2.0/Bloknot.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+using System.IO;
+using System.Windows.Controls;
+using Microsoft.Win32;
+
+
+namespace Bloknot2._0
+{
+    public class Bloknot
+    {
+        string nameFile;//имя файла
+        RichTextBox fieldEdit;//Поле редактирования
+        public bool IsVisible { get; set; }
+        public bool Modified { get; set; }//Признак редактирования
+        public string NameFile
+        {
+            get { return nameFile; }
+        }
+        public Bloknot(RichTextBox fieldEdit)
+        {
+            nameFile = "";
+            this.fieldEdit = fieldEdit;
+            Modified = false;
+        }
+        public void CreateNewFile()
+        {
+            CheckingIsModified();
+            fieldEdit.Document.Blocks.Clear();
+            nameFile = "";
+            Modified = false;
+        }
+
+        public bool CheckingIsModified()
+        {
+            if (Modified == true)
+            {
+                MessageBoxResult result;
+                result = MessageBox.Show("Вы хотите сохранить изменения в файле?", "Блокнот", MessageBoxButton.YesNoCancel);
+                if (result == MessageBoxResult.Yes)
+                {
+                    if (ASaveBloknot() == false) return false;
+                }
+                if (result == MessageBoxResult.Cancel) return false;
+            }
+            return true;
+        }
+        bool result2;
+        public void Save()
+        {
+            if (File.Exists(NameFile))
+            {
+                string path = Path.GetDirectoryName(NameFile);
+                MessageBoxResult result1;
+                result1 = MessageBox.Show("Вы желаете сохранить изменения в файле?", "Блокнот", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (result1 == MessageBoxResult.Yes)
+                {
+                    TextRange documentTextRange = new TextRange(fieldEdit.Document.ContentStart, fieldEdit.Document.ContentEnd);
+                    using (FileStream fs = File.Create(nameFile))
+                    {
+                        if (System.IO.Path.GetExtension(nameFile).ToLower() == ".rtf")
+                        {
+                            documentTextRange.Save(fs, DataFormats.Text);
+                            result2 = true;
+                        }
+                    }
+                }
+                if (result1 == MessageBoxResult.No)
+                {
+                    result2 = false;
+                }
+
+            }
+            else
+            {
+                MessageBoxResult result;
+                result = MessageBox.Show("Вы желаете сохранить новый файл?", "Сохрание", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (result == MessageBoxResult.Yes)
+                {
+                    ASaveBloknot();
+                }
+            }
+        }
+        public bool ASaveBloknot()
+        {
+            SaveFileDialog sd = new SaveFileDialog();
+            sd.DefaultExt = "rtf";
+            sd.Filter = "Текстовый файл(*.rtf)|*.rtf|Все файлы(*.*)|*.*";
+            if (nameFile == "")
+            {
+                if (sd.ShowDialog() == true)
+                {
+                    nameFile = sd.FileName;
+                    TextRange doc = new TextRange(fieldEdit.Document.ContentStart, fieldEdit.Document.ContentEnd);
+                    FileStream fs = File.Create(sd.FileName);
+                    doc.Save(fs, DataFormats.Rtf);
+                    fs.Close();
+                    Modified = false;
+                    return true;
+                }
+                else return false;
+            }
+            else
+            {
+                if (sd.ShowDialog() == true)
+                {
+                    nameFile = sd.FileName;
+                    TextRange doc = new TextRange(fieldEdit.Document.ContentStart, fieldEdit.Document.ContentEnd);
+                    FileStream fs = File.Create(sd.FileName);
+                    doc.Save(fs, DataFormats.Rtf);
+                    fs.Close();
+                    Modified = false;
+                    return true;
+                }
+            }
+            Modified = false;
+            return true;
+        }
+
+        public bool Open()
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Текстовый файл(*.rtf)|*.rtf|Все файлы(*.*)|*.*";
+            open.DefaultExt = "rtf";
+            open.Title = "Открытие файла";
+            if (open.ShowDialog() == true)
+            {
+                nameFile = open.FileName;
+                TextRange doc = new TextRange(fieldEdit.Document.ContentStart, fieldEdit.Document.ContentEnd);
+                FileStream fs = File.OpenRead(open.FileName);
+                doc.Load(fs, DataFormats.Rtf);
+                fs.Close();
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Bloknot2.0/MainWindow.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Bloknot2._0
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    
+    public partial class MainWindow : Window
+    {
+        Bloknot bloknot;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            bloknot = new Bloknot(richTextBox);
+        }
+
+        private void Create_Click(object sender, RoutedEventArgs e)
+        {
+            if (bloknot.CheckingIsModified() == false) return;
+            bloknot.CreateNewFile();
+            this.Title = bloknot.NameFile;
+        }
+
+        private void RichTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            bloknot.Modified = true;
+        }
+
+        private void newWindow_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow newWindow = new MainWindow();
+            newWindow.Show();
+        }
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            if (bloknot.CheckingIsModified() == false) return;
+            if (bloknot.Open() == false) return;
+            this.Title = bloknot.NameFile;
+        }
+
+        private void SaveAs_Click(object sender, RoutedEventArgs e)
+        {
+            bloknot.ASaveBloknot();
+            this.Title = bloknot.NameFile;
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            if (bloknot.CheckingIsModified() == false) return;
+            this.Close();
+        }
+
+        private void StatusBar_Click(object sender, RoutedEventArgs e)
+        {
+            if (statusBar.Visibility == Visibility.Visible)
+            {
+                statusBar.Visibility = Visibility.Collapsed;
+                richTextBox.Margin = new Thickness(0, 0, 0, 0);
+            }
+            else
+            {
+                statusBar.Visibility = Visibility.Visible;
+                richTextBox.Margin = new Thickness(0, 0, 0, 22);
+            }
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            if (bloknot.NameFile == "")
+            {
+                bloknot.ASaveBloknot();
+                this.Title = bloknot.NameFile;
+            }
+            else
+            {
+                bloknot.Save();
+            }
+            this.Title = bloknot.NameFile;
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            richTextBox.Undo();
+        }
+
+        private void Cut_Click(object sender, RoutedEventArgs e)
+        {
+            richTextBox.Cut();
+        }
+
+        private void Copy_Click(object sender, RoutedEventArgs e)
+        {
+            richTextBox.Copy();
+        }
+
+        private void Paste_Click(object sender, RoutedEventArgs e)
+        {
+            richTextBox.Paste();
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            richTextBox.Selection.Text = "";
+        }
+        private void SelectAll_Click(object sender, RoutedEventArgs e)
+        {
+            richTextBox.SelectAll();
+        }
+        private void ChangeLines_Click(object sender, RoutedEventArgs e)
+        {
+            Task task = new Task(richTextBox);
+            task.Show();
+        }
+        DispatcherTimer timer;
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            timer = new DispatcherTimer();
+            timer.Tick += Timer_Tick;
+            timer.Interval = new TimeSpan(0, 0, 0, 1, 0);
+            timer.IsEnabled = true;
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            DateTime d = DateTime.Now;
+            time.Text = d.ToString("HH:mm:ss");
+            data.Text = d.ToString("dd:MM:yyyy");
+        }
+    }
+}
